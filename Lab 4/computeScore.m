7972a7c911057159b855201d522a7aeacb666a0f
Lukas Gemar <Lukas Gemar>
2015-12-03 09:12:53 -0500

    PID controller is working beautifully

---
+++ Lab 4/computeScore.m
@@ -0,0 +1,18 @@
+function score = computeScore(model_name)
+
+% Run the simulink file from Matlab 
+sim(model_name)   
+
+% Read in the simulink output
+% Grab theata from the workspace
+theta = theta_out.Data; 
+max_theta = max(theta(:));
+
+% Grab x position data from the workspace
+t = x_out.Time; 
+temp = x_out.Data;
+y = reshape(temp,length(t),1);
+info = stepinfo(y,t, 'SettlingTimeThreshold', 0.01); 
+
+% Compute the score
+score = 155 - 10 * info.Overshoot - 2 * info.SettlingTime - 500 * max_theta; 
