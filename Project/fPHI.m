483ab1a77ec734c55b3c548d29ba46de5d373724
tjesch <tesch@college.harvard.edu>
2015-12-05 12:37:00 -0500

    modified beta

--- Project/fPHI.m
+++ Project/fPHI.m
@@ -1,3 +1,3 @@
 function PHI = fPHI(tf,tc,P)
 
-PHI = P.r*P.gamma*(fFun(0)-fFun(tf)+(1/beta)*fFun(0)*(1+exp(beta*tf)) -(2/beta)*fFun(tc)*(1+exp(-beta*(tf-tc))));
+PHI = P.r*P.gamma*(fFun(0)-fFun(tf)+(1/P.beta)*fFun(0)*(1+exp(P.beta*tf)) -(2/P.beta)*fFun(tc)*(1+exp(-P.beta*(tf-tc))));
